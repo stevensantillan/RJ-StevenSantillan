69b062b85e13c2b18a1252546245397ab5b7748e
stevensantillan <stevensantillan2@gmail.com>
2022-08-23 13:23:08 -0300

    componentes1v2

--- src/components/cartComponents/ItemListContainer.js
+++ src/components/cartComponents/ItemListContainer.js
@@ -3,14 +3,14 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
-const ItemListContainer = () => {
+const ItemListContainer = ({titulo = "Este será nuestro futuro catálogo"}) => {
   return (
     <>
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="lg">
         <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
-            <h1 style={{textAlign: "center"}}>Nuestro fúturo catálogo</h1>    
+            <h1 style={{textAlign: "center"}}>{titulo}</h1>    
         </Box> 
         <Box/>
       </Container>
