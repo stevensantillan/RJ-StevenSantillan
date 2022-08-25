7b501a36687ea115c7d5278c479f3976ab297469
stevensantillan <stevensantillan2@gmail.com>
2022-08-25 13:51:32 -0300

    componentes2

--- src/components/items/ItemCount.js
+++ src/components/items/ItemCount.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -6,36 +6,84 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import { useState } from 'react';
 
-const ItemCount = () => {
+const ItemCount = ({stock}) => {
 
-    let counter = 1
+    const [initial, setInitial ] = useState(1)
+    const [AddToCart, setAddCart] = useState(true)
+
+
+    const handleOnAdd = () => {
+        if(initial < stock){
+            setInitial( initial + 1 )
+        }
+      
+    }
+  
+    const handleOnDecrement = () => {
+        if(initial > 1){
+            setInitial( initial - 1 )
+        }
+    }
+
+    const handleAddToCart = () => {
+        setAddCart(!AddToCart)
+    }
+
+    useEffect (
+        () => {
+            console.log('Se han añadido ' + initial + ' al carrito')
+        }, [AddToCart]
+    )
 
   return (
-    
+
     <Card sx={{ width: "auto", m:2 }}>
     <CardMedia
-    component="img"
-    alt="example"
-    height="140"
-    image="#"
+        component="img"
+        alt="example"
+        height="140"
+        image="#"
     />
     <CardContent>
-    <Typography gutterBottom variant="h5" component="div">
-        Producto de Ejemplo
+    <Typography 
+        gutterBottom variant="h5" 
+        component="div">
+            Producto de Ejemplo
     </Typography>
-    <Typography variant="body2" color="text.secondary">
+    <Typography 
+        variant="body2" 
+        color="text.secondary">
         Card example, pronto estará nuestro producto en esta sección de detalle.
     </Typography>
     </CardContent>
     <CardActions>
-    <Box display="flex" justifyContent="center" sx={{ width: '100%'}}>
-        <Button variant="outlined" color="secondary" sx={{mr: 1, height: 30}}>-</Button>  
-        <Box border={1} sx={{ width: '10%', height: 30, borderColor: 'secondary.main', pt:'1%', color: 'secondary.main'}} style={{textAlign: "center"}}>
-            <span>{counter}</span> 
-        </Box>
-        <Button variant="outlined" color="secondary" sx={{ml: 1, height: 30 }}>+</Button>
-        <Button variant="contained" color="success" sx={{ml: 1, height: 30 }}>Add to cart</Button>
+    <Box 
+        display="flex" 
+        justifyContent="center" 
+        sx={{ width: '100%'}}>
+            <Button 
+                onClick={handleOnDecrement} 
+                variant="outlined" 
+                color="secondary" 
+                sx={{mr: 1, height: 30}}>-</Button>  
+            <Box 
+                border={1} 
+                sx={{ width: '10%', height: 30, borderColor: 'secondary.main', pt:'1%', color: 'secondary.main'}} 
+                style={{textAlign: "center"}}>
+                    <span>{initial}</span> 
+            </Box>
+        <Button 
+            onClick={handleOnAdd} 
+            variant="outlined" 
+            color="secondary" 
+            sx={{ml: 1, height: 30 }}>+</Button>
+        <Button 
+            onClick={handleAddToCart}
+            variant="contained" 
+            color="success" 
+            sx={{ml: 1, height: 30 }}>Add to Cart</Button>
     </Box>
     </CardActions>
     </Card>
--- src/components/items/ItemListContainer.js
+++ src/components/items/ItemListContainer.js
@@ -6,21 +6,30 @@ import ItemCount from './ItemCount';
 
 const ItemListContainer = ({titulo = "Este será nuestro futuro catálogo"}) => {
   
-  
+  const fakeProd = {
+    id: 1,
+    price: 300,
+    nameProd: "Example",
+    detail: "lorem",
+    stock: 6
+  }
+
   return (
     <>
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="false">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+        <Box 
+          sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
 
-          <h1 style={{textAlign: "center"}}>{titulo}</h1>  
+            <h1 style={{textAlign: "center"}}>{titulo}</h1>  
 
-          <Box sx={{ bgcolor: '#cfe8fc', display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gridTemplateRows: 'repeat(3, 1fr)'}}>
-            <ItemCount/>
-          </Box> 
+          <Box 
+            sx={{ bgcolor: '#cfe8fc', display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gridTemplateRows: 'repeat(3, 1fr)'}}>
+            
+            <ItemCount stock={6}/>
           
-
+          </Box> 
         </Box> 
       </Container> 
     </React.Fragment>
